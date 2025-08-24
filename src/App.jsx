4c2da32524af926e466a3212b204b3cc4022af12
selvaraj <selvaraj1662003@gmail.com>
2025-08-24 08:47:53 +0530

    Changes code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Hero from './sections/Hero.jsx';
-// import About from './sections/About.jsx';
 // import Footer from './sections/Footer.jsx';
 import Navbar from './sections/Navbar.jsx';
+import About from './sections/About.jsx';
 // import Contact from './sections/Contact.jsx';
 // import Clients from './sections/Clients.jsx';
 // import Projects from './sections/Projects.jsx';
@@ -12,6 +12,7 @@ const App = () => {
     <main className="max-w-7xl mx-auto relative">
       <Navbar />
       <Hero />
+      <About />
     </main>
   );
 };
